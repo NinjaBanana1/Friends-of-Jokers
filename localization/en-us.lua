2609e24f026407a52543175a5a1301289e9567c3
neoncube <neoncubedev@proton.me>
2024-12-13 19:14:48 +0330

    Add The Witcher subset jokers Add metadata manifest Add main mod file (FriendsOfJokers.lua) Update README.md Add placeholder atlases Add English localization file

---
+++ localization/en-us.lua
@@ -0,0 +1,36 @@
+return {
+    descriptions = {
+        Joker = {
+            j_foj_witcher1 = {
+                name = "Keira Mietz",
+                text = {
+                    "{C:attention}#1#{} Consumable Slot,",
+                    "creates a copy of {C:attention}The Fool{}",
+                    "when Blind is selected",
+                    "{C:inactive}(Must have room)"
+                }
+            },
+            j_foj_witcher2 = {
+                name = "King of Beggars",
+                text = {
+                    "While {C:attention}in a Blind{}, earn",
+                    "{C:money}$#1#{} when a card is {C:attention}sold{}"
+                }
+            },
+            j_foj_witcher3 = {
+                name = "Casimir Bassi",
+                text = {
+                    "When obtained, {C:attention}set money to $0{}",
+                    "and gain {C:chips}+#1#{} Chips",
+                    "for every {C:money}$1{} removed",
+                    "{C:inactive}(Currently {C:chips}+#2#{C:inactive} chips)"
+                }
+            },
+        }
+    },
+    misc = {
+        v_dictionary = {
+            foj_consumable_slots_minus = "-#1# Consumable Slots", 
+        }
+    }
+}
