5bf230380601d87d9e3e399fede07d35e5444c3c
NinjaBanana1 <rileyjpep@gmail.com>
2025-06-13 22:09:14 -0400

    Update define.lua

--- lib/define.lua
+++ lib/define.lua
@@ -2,37 +2,37 @@
 --FOJ.config = FOJ.current_mod.config
 
 FOJ.JOKER_SET_1_2 = {
-  --  "AmoungUs",
+  --  "AmongUs",
   --  "Cyberpunk2077",
-  --  "DaveTheDiver",
+    "DaveTheDiver",
   --  "SlayTheSpire",
-   -- "StardewValley",
-   -- "TheBindingOfIsaac",
-    "TheWithcher",
+  --  "StardewValley",
+  -- "TheBindingOfIsaac",
+    "TheWitcher",
   --  "VampireSurvivors",
   --"",
 }
 
 FOJ.JOKER_SET_3 = {
-    "1000xRESIST",
-    "CultOfTheLamb",
-    "Divinity",
-    "DontStarve",
-    "EnterTheGungeon",
-    "PotionCraft",
-    "ShovelKnight",
-    "Warframe",
+  --  "1000xRESIST",
+  --  "CultOfTheLamb",
+  --  "Divinity",
+  --  "DontStarve",
+  --  "EnterTheGungeon",
+  --  "PotionCraft",
+  --  "ShovelKnight",
+  --  "Warframe",
   --"",
 }
 
 FOJ.JOKER_SET_4 = {
-    "AssassinsCreed",
-    "Bugsnax",
-    "Civilization",
-    "CriticalRole",
-    "DeadByDaylight",
-    "Fallout",
-    "Rust",
-    "SlayThePrincess",
+  --  "AssassinsCreed",
+  --  "Bugsnax",
+  --  "Civilization",
+  --  "CriticalRole",
+  --  "DeadByDaylight",
+  --  "Fallout",
+  --  "Rust",
+  --  "SlayThePrincess",
   --"",
 }
